4a712b99d8629b97b43742dee029933f3b9257a6
Vlado Tešanović <vlado.tesanovic@gmail.com>
2017-12-19 16:46:39 +0100

    Facade Pattern

--- src/structural/facade/facade.spec.ts
+++ src/structural/facade/facade.spec.ts
@@ -1,24 +1,24 @@
 import { ProjectileMotionFacade } from './facade';
 
 describe('Calculating components of projectile motion', () => {
-    let projectileMotion;
+  let projectileMotion;
 
-    beforeEach(() => {
-        projectileMotion = new ProjectileMotionFacade();
-    });
+  beforeEach(() => {
+    projectileMotion = new ProjectileMotionFacade();
+  });
 
-    it('velocity, distance and height after some time', () => {
-        const mock = {
-            initialVelocity: 13,
-            angle: 30,
-            time: 5,
-            result: {
-                velocity: 44.014230653278496,
-                height: 2.1534148827726804,
-                distance: 14.919295946949042
-            }
-        };
+  it('velocity, distance and height after some time', () => {
+    const mock = {
+      initialVelocity: 13,
+      angle: 30,
+      time: 5,
+      result: {
+        velocity: 44.014230653278496,
+        height: 2.1534148827726804,
+        distance: 14.919295946949042
+      }
+    };
 
-        expect(projectileMotion.calculate(mock.initialVelocity, mock.angle, mock.time)).toEqual(mock.result);
-    });
+    expect(projectileMotion.calculate(mock.initialVelocity, mock.angle, mock.time)).toEqual(mock.result);
+  });
 });
